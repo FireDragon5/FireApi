72846b8e43e8646e26fee86dcd106ac4541546c5
Anthonie <20230585@ctucareer.co.za>
2023-07-28 14:47:12 +0200

    Added comments to make the code more readable

--- src/main/java/me/firedraong5/firesapi/menu/Menu.java
+++ src/main/java/me/firedraong5/firesapi/menu/Menu.java
@@ -19,61 +19,132 @@ public class Menu implements Listener {
 	private int size = 9;
 	private boolean slotNumbersVisible = false;
 
+
+	/**
+	 * Create a new menu
+	 *
+	 * @param player Player to open the menu
+	 * @param name   Name of the menu
+	 * @param size   Size of the menu
+	 */
 	public Menu(Player player, String name, int size) {
 		this.player = player;
 		this.title = UtilsMessage.onChat(name);
 		this.size = size;
+
 		this.inventory = Bukkit.createInventory(player, size, title);
+
 	}
 
+
+	/**
+	 * Create a new menu
+	 */
+	public Menu() {
+		this.player = null;
+		inventory = null;
+	}
+
+	/**
+	 * Get the player
+	 * @return Player
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 
+	/**
+	 * Get the inventory
+	 * @return Inventory
+	 */
 	public String getName() {
 		return title;
 	}
 
+	/**
+	 * Set the name of the menu
+	 * @param name Name of the menu
+	 * @return Menu
+	 */
 	public Menu setName(String name) {
 		this.title = UtilsMessage.onChat(name);
 		return this;
 	}
 
+	/**
+	 * Get the size of the menu
+	 * @return Size of the menu
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * Set the size of the menu
+	 * @param size Size of the menu
+	 * @return Menu
+	 */
 	public Menu setSize(int size) {
 		this.size = size;
 		return this;
 	}
 
+	/**
+	 * Get if the slot numbers are visible
+	 * @return Slot numbers visible
+	 */
 	public boolean isSlotNumbersVisible() {
 		return slotNumbersVisible;
 	}
 
+	/**
+	 * Set if the slot numbers are visible
+	 * @param slotNumbersVisible Slot numbers visible
+	 * @return Menu
+	 */
 	public Menu setSlotNumbersVisible(boolean slotNumbersVisible) {
 		this.slotNumbersVisible = slotNumbersVisible;
 		return this;
 	}
 
+	/**
+	 * Get the inventory
+	 * @return Inventory
+	 */
 	public ItemStack getItem(int slot) {
 		return inventory.getItem(slot);
 	}
 
+	/**
+	 * Set the inventory
+	 * @param slot Slot to set the item
+	 * @param item Item to set
+	 */
 	public void setItem(int slot, ItemStack item) {
 		inventory.setItem(slot, item);
 	}
 
+	/**
+	 * Set the inventory
+	 * @param slot Slot to set the item
+	 * @param item Item to set
+	 * @param name Name of the item
+	 */
 	public void setItem(int slot, ItemStack item, String name) {
 		item.getItemMeta().setDisplayName(UtilsMessage.onChat(name));
 		inventory.setItem(slot, item);
 	}
 
+	/**
+	 * Open the inventory
+	 */
 	public void openMenu() {
 		player.openInventory(inventory);
 	}
 
+	/**
+	 * When slotNumbers are turn turn it will show the slot numbers
+	 */
 	private void slotNumbers() {
 		if (slotNumbersVisible) {
 			for (int i = 0; i < size; i++) {
@@ -85,6 +156,10 @@ public class Menu implements Listener {
 		}
 	}
 
+	/**
+	 * When the inventory is clicked
+	 * @param event InventoryClickEvent
+	 */
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent event) {
 		if (event.getInventory() == null || !event.getInventory().equals(inventory)) return;
