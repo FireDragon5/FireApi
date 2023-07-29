c0fb80b5d05a1ddbe80cb465509cd6b34a4b8931
Anthonie <20230585@ctucareer.co.za>
2023-07-29 16:57:54 +0200

    Added a new Method

--- src/main/java/me/firedraong5/firesapi/itemCreation/CustomItemCreator.java
+++ src/main/java/me/firedraong5/firesapi/itemCreation/CustomItemCreator.java
@@ -5,85 +5,140 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 public class CustomItemCreator {
 
 //	Get the item name
-	public static String getItemName(ItemStack item) {
+
+	public static @NotNull String getItemName(@NotNull ItemStack item) {
 		return item.getItemMeta().getDisplayName();
 	}
 
 //	Get the item lore
-	public static String getItemLore(ItemStack item) {
+public static String getItemLore(@NotNull ItemStack item) {
 		return item.getItemMeta().getLore().toString();
 	}
 
 //	Get the item material
-	public static Material getItemMaterial(ItemStack item) {
+public static @NotNull Material getItemMaterial(@NotNull ItemStack item) {
 		return item.getType();
 	}
 
 //	Get the item amount
-	public static int getItemAmount(ItemStack item) {
+public static int getItemAmount(@NotNull ItemStack item) {
 		return item.getAmount();
 	}
 
 //	Get the item durability
-	public static int getItemDurability(ItemStack item) {
+public static int getItemDurability(@NotNull ItemStack item) {
 		return item.getDurability();
 	}
 
 //	Get the item meta
-	public static ItemMeta getItemMeta(ItemStack item) {
+public static ItemMeta getItemMeta(@NotNull ItemStack item) {
 		return item.getItemMeta();
 	}
 
-//	Get the item
+	/**
+	 * Get the item
+	 *
+	 * @param item Item to get
+	 * @return Item
+	 */
 	public static ItemStack getItem(ItemStack item) {
 		return item;
 	}
 
-//	Set the item name
-	public static void setItemName(ItemStack item, String name) {
+	/**
+	 * Set the item name
+	 *
+	 * @param item Item to set
+	 * @param name Name to set
+	 */
+	public static void setItemName(@NotNull ItemStack item, String name) {
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(name);
 		item.setItemMeta(meta);
 	}
 
-//	Set the item lore
-	public static void setItemLore(ItemStack item, String... lore) {
+	/**
+	 * Set the item lore
+	 *
+	 * @param item Item to set
+	 * @param lore Lore to set
+	 */
+	public static void setItemLore(@NotNull ItemStack item, List<String> lore) {
 		ItemMeta meta = item.getItemMeta();
-		meta.setLore(Arrays.asList(lore));
+
+		meta.setLore(UtilsMessage.onChat(lore));
 		item.setItemMeta(meta);
 	}
 
-//	Set the item material
-	public static void setItemMaterial(ItemStack item, Material material) {
+	/**
+	 * Set the item
+	 *
+	 * @param item Item to set
+	 * @param material Material of the item
+	 */
+	public static void setItemMaterial(@NotNull ItemStack item, Material material) {
 		item.setType(material);
 	}
 
-//	Set the item amount
-	public static void setItemAmount(ItemStack item, int amount) {
+	/**
+	 * Set the item amount
+	 * @param item Item to set
+	 * @param amount Amount to set
+	 */
+	public static void setItemAmount(@NotNull ItemStack item, int amount) {
 		item.setAmount(amount);
 	}
 
-//	Set the item durability
-	public static void setItemDurability(ItemStack item, int durability) {
+	/**
+	 * Set the item durability
+	 *
+	 * @param item Item to set
+	 * @param durability Durability to set
+	 */
+	public static void setItemDurability(@NotNull ItemStack item, int durability) {
 		item.setDurability((short) durability);
 	}
 
-//	Set the item meta
-	public static void setItemMeta(ItemStack item, ItemMeta meta) {
+	/**
+	 * Set the item meta
+	 * @param item Item to set
+	 * @param meta Meta to set
+	 */
+	public static void setItemMeta(@NotNull ItemStack item, ItemMeta meta) {
 		item.setItemMeta(meta);
 	}
 
-//	Set the item
+	/**
+	 * Set the item
+	 * @param item Item to set
+	 * @param newItem New item
+	 */
 	public static void setItem(ItemStack item, ItemStack newItem) {
 		item = newItem;
 	}
+
+	/**
+	 * Hide the NBT of the item
+	 *
+	 * @param item Item to hide
+	 */
+	public static void hideNBT(@NotNull ItemStack item) {
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(UtilsMessage.onChat(meta.getDisplayName()));
+		item.setItemMeta(meta);
+	}
+
 
 	/**
 	 * Create a new item
@@ -93,11 +148,11 @@ public class CustomItemCreator {
 	 * @param lore Lore of the item
 	 * @return The item
 	 */
-	public static ItemStack createItem(Material material, int amount, String name, String... lore) {
+	public static @NotNull ItemStack createItem(Material material, int amount, String name, List<String> lore) {
 		ItemStack item = new ItemStack(material, amount);
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(UtilsMessage.onChat(name));
-		meta.setLore(Collections.singletonList(UtilsMessage.onChat(Arrays.toString(lore))));
+		meta.setLore(UtilsMessage.onChat(lore));
 		item.setItemMeta(meta);
 		return item;
 	}
@@ -111,11 +166,11 @@ public class CustomItemCreator {
 	 * @param lore Lore of the item
 	 * @return The item
 	 */
-	public static ItemStack createItem(Material material, int amount, String name, int durability, String... lore) {
+	public static @NotNull ItemStack createItem(Material material, int amount, String name, int durability, List<String> lore) {
 		ItemStack item = new ItemStack(material, amount);
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(UtilsMessage.onChat(name));
-		meta.setLore(Collections.singletonList(UtilsMessage.onChat(Arrays.toString(lore))));
+		meta.setLore(UtilsMessage.onChat(lore));
 		item.setItemMeta(meta);
 		item.setDurability((short) durability);
 		return item;
@@ -131,12 +186,12 @@ public class CustomItemCreator {
 	 * @param playerMessage Message to send to the player
 	 * @return The item
 	 */
-	public static ItemStack createItemWithPlayerMessage(Player player,Material material, int amount, String name,
-														int durability, String playerMessage, String... lore) {
+	public static @NotNull ItemStack createItemWithPlayerMessage(Player player, Material material, int amount, String name,
+																 int durability, String playerMessage, List<String> lore) {
 		ItemStack item = new ItemStack(material, amount);
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(UtilsMessage.onChat(name));
-		meta.setLore(Collections.singletonList(UtilsMessage.onChat(Arrays.toString(lore))));
+		meta.setLore(UtilsMessage.onChat(lore));
 		item.setItemMeta(meta);
 		item.setDurability((short) durability);
 
@@ -153,7 +208,7 @@ public class CustomItemCreator {
 	 * @param player Player to add the item
 	 */
 
-	public static void addToInventory(Player player,ItemStack item) {
+	public static  void addToInventory(@NotNull Player player, ItemStack item) {
 		player.getInventory().addItem(item);
 	}
 
