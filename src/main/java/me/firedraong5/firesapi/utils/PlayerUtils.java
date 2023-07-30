be42e47445c8810ce1391d2197fefc3c7bfa4bcb
Anthonie <20230585@ctucareer.co.za>
2023-07-30 12:42:53 +0200

    Added a new method

--- src/main/java/me/firedraong5/firesapi/utils/PlayerUtils.java
+++ src/main/java/me/firedraong5/firesapi/utils/PlayerUtils.java
@@ -1,5 +1,6 @@
 package me.firedraong5.firesapi.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -13,6 +14,21 @@ public class PlayerUtils {
 	 */
 	public static void teleportPlayer(Player player, Location location) {
 		player.teleport(location);
+	}
+
+
+	/**
+	 * Get all the players from the server
+	 *
+	 * @return All the players from the server
+	 */
+	public static Player getAllPlayers() {
+
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			return player;
+		}
+
+		return null;
 	}
 
 
