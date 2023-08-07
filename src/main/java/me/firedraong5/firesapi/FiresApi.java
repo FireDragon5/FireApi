a98a2c2a978d143fb2aef73a3ebb9d729ecdd351
Anthonie Slabbert <20230585@ctucareer.co.za>
2023-08-07 09:27:06 +0200

    Some changes

--- src/main/java/me/firedraong5/firesapi/FiresApi.java
+++ src/main/java/me/firedraong5/firesapi/FiresApi.java
@@ -1,8 +1,12 @@
 package me.firedraong5.firesapi;
 
+import me.firedraong5.firesapi.command.FireCommand;
 import me.firedraong5.firesapi.menu.Menu;
 import me.firedraong5.firesapi.utils.UtilsMessage;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class FiresApi extends JavaPlugin {
 
@@ -10,6 +14,9 @@ public final class FiresApi extends JavaPlugin {
     public void onEnable() {
 
         getServer().getPluginManager().registerEvents(new UtilsMessage(), this);
+
+        FireCommand fireCommand = new FireCommand("test");
+        Objects.requireNonNull(getCommand("test")).setExecutor((CommandExecutor) fireCommand);
 
     }
 
